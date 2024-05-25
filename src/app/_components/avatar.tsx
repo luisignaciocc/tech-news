02c3c0fa0d7e07d1704ddb6da5709cf3596eb5b4
Luis Ignacio <luisignacioccp@gmail.com>
2024-05-24 20:14:52 -0400

    Add name

--- src/app/_components/avatar.tsx
+++ src/app/_components/avatar.tsx
@@ -8,7 +8,13 @@ type Props = {
 const Avatar = ({ name, picture }: Props) => {
   return (
     <div className="flex items-center">
-      <Image src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
+      <Image
+        src={picture}
+        className="w-12 h-12 rounded-full mr-4"
+        alt={name}
+        width={48}
+        height={48}
+      />
       <div className="text-xl font-bold">{name}</div>
     </div>
   );
--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,5 +1,4 @@
 import Container from "@/app/_components/container";
-import { EXAMPLE_PATH } from "@/lib/constants";
 
 export function Footer() {
   return (
@@ -7,20 +6,24 @@ export function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+            Automáticamente Generado con Inteligencia Artificial.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/app/building-your-application/routing/layouts-and-templates"
+              href="https://www.bocono-labs.com/work"
+              target="_blank"
+              rel="noopener noreferrer"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Read Documentation
+              Saber más
             </a>
             <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+              href={`https://github.com/luisignaciocc/tech-news`}
+              target="_blank"
+              rel="noopener noreferrer"
               className="mx-3 font-bold hover:underline"
             >
-              View on GitHub
+              Ver repo en GitHub
             </a>
           </div>
         </div>
