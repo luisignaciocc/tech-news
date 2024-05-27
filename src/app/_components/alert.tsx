c14c780822c2e419ab390e9e8933e21fd3d3073b
Luis Ignacio <luisignacioccp@gmail.com>
2024-05-27 19:31:05 -0400

    remove alert

--- src/app/_components/alert.tsx
+++ src/app/_components/alert.tsx
@@ -1,46 +1,21 @@
 import cn from "classnames";
 
 import Container from "@/app/_components/container";
-import { EXAMPLE_PATH } from "@/lib/constants";
 
 type Props = {
-  preview?: boolean;
+  text: string;
 };
 
-const Alert = ({ preview }: Props) => {
+const Alert = ({ text }: Props) => {
   return (
     <div
       className={cn("border-b", {
-        "bg-neutral-800 border-neutral-800 text-white": preview,
-        "bg-neutral-50 border-neutral-200": !preview,
+        "bg-neutral-800 border-neutral-800 text-white": true,
+        // "bg-neutral-50 border-neutral-200": !preview,
       })}
     >
       <Container>
-        <div className="py-2 text-center text-sm">
-          {preview ? (
-            <>
-              This page is a preview.{" "}
-              <a
-                href="/api/exit-preview"
-                className="underline hover:text-teal-300 duration-200 transition-colors"
-              >
-                Click here
-              </a>{" "}
-              to exit preview mode.
-            </>
-          ) : (
-            <>
-              The source code for this blog is{" "}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-blue-600 duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
-            </>
-          )}
-        </div>
+        <div className="py-2 text-center text-sm">{text}</div>
       </Container>
     </div>
   );
