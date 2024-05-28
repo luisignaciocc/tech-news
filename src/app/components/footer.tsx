652be132356d43eb3b9d220187dd596d796dea57
Luis Ignacio <luisignacioccp@gmail.com>
2024-05-28 12:52:49 -0400

    fix h1

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -5,9 +5,9 @@ export function Footer() {
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+          <h1 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             Automáticamente Generado con Inteligencia Artificial.
-          </h3>
+          </h1>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="https://www.bocono-labs.com/work"
