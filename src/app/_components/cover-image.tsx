a3cd11cf0277527c86a08cdd87063439bacac731
Luis Ignacio <luisignacioccp@gmail.com>
2024-05-27 20:12:19 -0400

    remove custom styles

--- src/app/_components/cover-image.tsx
+++ src/app/_components/cover-image.tsx
@@ -14,8 +14,8 @@ const CoverImage = ({ title, src, slug }: Props) => {
     <img
       src={src}
       alt={`Cover Image for ${title}`}
-      className={cn("shadow-sm w-full", {
-        "hover:shadow-lg transition-shadow duration-200": slug,
+      className={cn("shadow-lg w-full", {
+        "hover:shadow-xl transition-shadow duration-200": slug,
       })}
       width={1300}
       height={630}
--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             Automáticamente Generado con Inteligencia Artificial.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
