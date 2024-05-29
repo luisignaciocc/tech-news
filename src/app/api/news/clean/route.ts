c535f430b71c83314c2eec64bd481be496739150
Luis Ignacio <luisignacioccp@gmail.com>
2024-05-29 19:15:33 -0400

    add clean script

---
+++ src/app/api/news/clean/route.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request): Promise<NextResponse> {
+  const apiKey = request.headers.get("x-api-key");
+
+  if (apiKey !== process.env.API_KEY) {
+    return NextResponse.json({ error: "Invalid API key" }, { status: 401 });
+  }
+
+  try {
+    const prisma = new PrismaClient();
+
+    await prisma.news.deleteMany({
+      where: {
+        createdAt: {
+          lt: new Date(Date.now() - 1000 * 60 * 60 * 24 * 4),
+        },
+        posts: {
+          none: {},
+        },
+      },
+    });
+
+    return NextResponse.json({ success: true }, { status: 200 });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { error: `Error al hacer la solicitud a la API: ${error.message}` },
+        { status: 500 },
+      );
+    } else {
+      return NextResponse.json(
+        { error: "Error al hacer la solicitud a la API" },
+        { status: 500 },
+      );
+    }
+  }
+}
