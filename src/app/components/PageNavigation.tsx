5452f228e76180f63bef0ad877fedbb3ad4b63c1
ErickBackDev <erickson17graterol@gmail.com>
2024-05-29 14:41:36 -0400

    changing PageNavigation from client component to server component

--- src/app/components/PageNavigation.tsx
+++ src/app/components/PageNavigation.tsx
@@ -1,6 +1,4 @@
-"use client";
-import { useRouter } from "next/navigation";
-
+import Link from "next/link";
 interface PageNavigationProps {
   currentPage: string;
   hasMorePosts: boolean;
@@ -11,34 +9,23 @@ export default function PageNavigation({
   hasMorePosts,
 }: PageNavigationProps) {
   const page = parseInt(currentPage);
-  const router = useRouter();
-
-  const handlePageChange = (newPage: number) => {
-    if (newPage > 1) {
-      router.push(`/record/${newPage}`);
-    } else {
-      router.push("/");
-    }
-  };
 
   return (
     <div>
       <div className="flex justify-between mb-5">
         {page > 1 && (
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => handlePageChange(page - 1)}
-          >
-            Anterior
-          </button>
+          <Link href={page > 2 ? `/record/${page - 1}` : "/"}>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Anterior
+            </button>
+          </Link>
         )}
         {hasMorePosts && (
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => handlePageChange(page + 1)}
-          >
-            Siguiente
-          </button>
+          <Link href={`/record/${page + 1}`}>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Siguiente
+            </button>
+          </Link>
         )}
       </div>
     </div>
