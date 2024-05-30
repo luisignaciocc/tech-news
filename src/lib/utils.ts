caf21f4425eb27758c85246dd863b93e3dfadf92
Luis Ignacio <luisignacioccp@gmail.com>
2024-05-30 10:20:48 -0400

    update pagination

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const PER_PAGE = 10;
+export const PER_PAGE = 30;
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
