86033eab17156c636115fe03f632ea7bf623a8bc
Luis Ignacio <luisignacioccp@gmail.com>
2024-05-31 12:13:05 -0400

    Add manifest

---
+++ src/lib/constants.ts
@@ -0,0 +1,3 @@
+export const SITE_SHORT_NAME = `Tecnobuc`;
+export const SITE_NAME = `${SITE_SHORT_NAME} | Noticiero de tecnología generado por IA`;
+export const SITE_DESCRIPTION = `Un noticiero digital de tecnología con contenido generado automáticamente.`;
