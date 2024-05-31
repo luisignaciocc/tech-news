290ecc6020cfd61178757ef91d9cc2d964aa707d
ErickBackDev <erickson17graterol@gmail.com>
2024-05-31 16:17:53 -0400

    encrypting passwords and handling errors in the server component

--- src/app/auth/register/actions.ts
+++ src/app/auth/register/actions.ts
@@ -1,8 +1,7 @@
 "use server";
 
 import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import bcrypt from "bcrypt";
 
 interface RegisterData {
   username: string;
@@ -11,31 +10,44 @@ interface RegisterData {
 }
 
 export async function registerUser(data: RegisterData) {
-  const usernameFound = await prisma.users.findUnique({
-    where: {
-      username: data.username,
-    },
-  });
+  try {
+    const prisma = new PrismaClient();
 
-  if (usernameFound) {
-    throw new Error("User already exists");
+    const usernameFound = await prisma.users.findUnique({
+      where: {
+        username: data.username,
+      },
+    });
+
+    if (usernameFound) {
+      throw new Error("User already exists");
+    }
+
+    const emailFound = await prisma.users.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (emailFound) {
+      throw new Error("Email already exists");
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    await prisma.users.create({
+      data: {
+        username: data.username,
+        email: data.email,
+        password: hashedPassword,
+      },
+    });
+
+    return { success: true, message: "User registered successfully" };
+  } catch (error) {
+    return {
+      success: false,
+      message: "An error occurred while registering the user",
+    };
   }
-
-  const emailFound = await prisma.users.findUnique({
-    where: {
-      email: data.email,
-    },
-  });
-
-  if (emailFound) {
-    throw new Error("Email already exists");
-  }
-
-  const newUser = await prisma.users.create({
-    data,
-  });
-
-  const { password: _, ...user } = newUser;
-
-  return user;
 }
