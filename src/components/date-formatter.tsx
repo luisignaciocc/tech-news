7edfd0a251181b5e9fe1178769e6ec4e764899d3
Luis Ignacio <luisignacioccp@gmail.com>
2024-05-31 22:19:19 -0400

    format to locale date

--- src/components/date-formatter.tsx
+++ src/components/date-formatter.tsx
@@ -1,13 +1,18 @@
 import { format } from "date-fns";
+import { toZonedTime } from "date-fns-tz";
 
 type Props = {
   date: Date;
 };
 
 const DateFormatter = ({ date }: Props) => {
-  return (
-    <time dateTime={date.toISOString()}>{format(date, "LLLL	d, yyyy")}</time>
-  );
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  const zonedDate = toZonedTime(date, timeZone);
+
+  const formattedDate = format(zonedDate, "LLLL d, yyyy");
+
+  return <time dateTime={date.toISOString()}>{formattedDate}</time>;
 };
 
 export default DateFormatter;
