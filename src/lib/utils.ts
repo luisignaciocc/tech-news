f4425f65415520b64325d4c69093376a2cb5ff4e
Luis Ignacio <luisignacioccp@gmail.com>
2024-06-02 08:47:57 -0400

    chore: Add WhatsApp notification for publishing errors

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,20 @@ export const PER_PAGE = 30;
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export async function notifyProblem(type: string, error: unknown) {
+  const API_KEY = process.env.CALLMEBOT_API_KEY_IMPORTANT;
+  const PHONE = process.env.WHATSAPP_PHONE_IMPORTANT;
+
+  if (!API_KEY || !PHONE) {
+    console.error("No whatsapp API keys found");
+    throw new Error("No whatsapp API keys found");
+  }
+
+  console.error(error);
+  await fetch(
+    `https://api.callmebot.com/whatsapp.php?phone=${PHONE}&text=${encodeURIComponent(
+      `A proble occurred while Publishing the ${type} post. Check the logs for more information.`,
+    )}&apikey=${API_KEY}`,
+  );
+}
