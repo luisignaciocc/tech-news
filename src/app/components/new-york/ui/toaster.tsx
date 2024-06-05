cd3c6d74d4ceb18de5b674f4bfd616b5360061c9
ErickBackDev <erickson17graterol@gmail.com>
2024-06-05 13:47:21 -0400

    create default dashboard

--- src/app/components/new-york/ui/toaster.tsx
+++ src/app/components/new-york/ui/toaster.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {
   Toast,
@@ -7,11 +7,11 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/registry/new-york/ui/toast"
-import { useToast } from "@/registry/new-york/ui/use-toast"
+} from "@/registry/new-york/ui/toast";
+import { useToast } from "@/registry/new-york/ui/use-toast";
 
 export function Toaster() {
-  const { toasts } = useToast()
+  const { toasts } = useToast();
 
   return (
     <ToastProvider>
@@ -27,9 +27,9 @@ export function Toaster() {
             {action}
             <ToastClose />
           </Toast>
-        )
+        );
       })}
       <ToastViewport />
     </ToastProvider>
-  )
+  );
 }
--- src/lib/shadcn/rehype-component.ts
+++ src/lib/shadcn/rehype-component.ts
@@ -1,11 +1,11 @@
-import fs from "fs"
-import path from "path"
-import { UnistNode, UnistTree } from "types/unist"
-import { u } from "unist-builder"
-import { visit } from "unist-util-visit"
+import fs from "fs";
+import path from "path";
+import { UnistNode, UnistTree } from "types/unist";
+import { u } from "unist-builder";
+import { visit } from "unist-util-visit";
 
-import { Index } from "../__registry__"
-import { styles } from "../registry/styles"
+import { Index } from "../__registry__";
+import { styles } from "../registry/styles";
 
 export function rehypeComponent() {
   return async (tree: UnistTree) => {
@@ -13,51 +13,51 @@ export function rehypeComponent() {
       // src prop overrides both name and fileName.
       const { value: srcPath } =
         (getNodeAttributeByName(node, "src") as {
-          name: string
-          value?: string
-          type?: string
-        }) || {}
+          name: string;
+          value?: string;
+          type?: string;
+        }) || {};
 
       if (node.name === "ComponentSource") {
-        const name = getNodeAttributeByName(node, "name")?.value as string
+        const name = getNodeAttributeByName(node, "name")?.value as string;
         const fileName = getNodeAttributeByName(node, "fileName")?.value as
           | string
-          | undefined
+          | undefined;
 
         if (!name && !srcPath) {
-          return null
+          return null;
         }
 
         try {
           for (const style of styles) {
-            let src: string
+            let src: string;
 
             if (srcPath) {
-              src = srcPath
+              src = srcPath;
             } else {
-              const component = Index[style.name][name]
+              const component = Index[style.name][name];
               src = fileName
                 ? component.files.find((file: string) => {
                     return (
                       file.endsWith(`${fileName}.tsx`) ||
                       file.endsWith(`${fileName}.ts`)
-                    )
+                    );
                   }) || component.files[0]
-                : component.files[0]
+                : component.files[0];
             }
 
             // Read the source file.
-            const filePath = path.join(process.cwd(), src)
-            let source = fs.readFileSync(filePath, "utf8")
+            const filePath = path.join(process.cwd(), src);
+            let source = fs.readFileSync(filePath, "utf8");
 
             // Replace imports.
             // TODO: Use @swc/core and a visitor to replace this.
             // For now a simple regex should do.
             source = source.replaceAll(
               `@/registry/${style.name}/`,
-              "@/components/"
-            )
-            source = source.replaceAll("export default", "export")
+              "@/components/",
+            );
+            source = source.replaceAll("export default", "export");
 
             // Add code as children so that rehype can take over at build time.
             node.children?.push(
@@ -88,38 +88,38 @@ export function rehypeComponent() {
                     ],
                   }),
                 ],
-              })
-            )
+              }),
+            );
           }
         } catch (error) {
-          console.error(error)
+          console.error(error);
         }
       }
 
       if (node.name === "ComponentPreview") {
-        const name = getNodeAttributeByName(node, "name")?.value as string
+        const name = getNodeAttributeByName(node, "name")?.value as string;
 
         if (!name) {
-          return null
+          return null;
         }
 
         try {
           for (const style of styles) {
-            const component = Index[style.name][name]
-            const src = component.files[0]
+            const component = Index[style.name][name];
+            const src = component.files[0];
 
             // Read the source file.
-            const filePath = path.join(process.cwd(), src)
-            let source = fs.readFileSync(filePath, "utf8")
+            const filePath = path.join(process.cwd(), src);
+            let source = fs.readFileSync(filePath, "utf8");
 
             // Replace imports.
             // TODO: Use @swc/core and a visitor to replace this.
             // For now a simple regex should do.
             source = source.replaceAll(
               `@/registry/${style.name}/`,
-              "@/components/"
-            )
-            source = source.replaceAll("export default", "export")
+              "@/components/",
+            );
+            source = source.replaceAll("export default", "export");
 
             // Add code as children so that rehype can take over at build time.
             node.children?.push(
@@ -142,11 +142,11 @@ export function rehypeComponent() {
                     ],
                   }),
                 ],
-              })
-            )
+              }),
+            );
           }
         } catch (error) {
-          console.error(error)
+          console.error(error);
         }
       }
 
@@ -256,24 +256,24 @@ export function rehypeComponent() {
       //     })
       //   )
       // }
-    })
-  }
+    });
+  };
 }
 
 function getNodeAttributeByName(node: UnistNode, name: string) {
-  return node.attributes?.find((attribute) => attribute.name === name)
+  return node.attributes?.find((attribute) => attribute.name === name);
 }
 
 function getComponentSourceFileContent(node: UnistNode) {
-  const src = getNodeAttributeByName(node, "src")?.value as string
+  const src = getNodeAttributeByName(node, "src")?.value as string;
 
   if (!src) {
-    return null
+    return null;
   }
 
   // Read the source file.
-  const filePath = path.join(process.cwd(), src)
-  const source = fs.readFileSync(filePath, "utf8")
+  const filePath = path.join(process.cwd(), src);
+  const source = fs.readFileSync(filePath, "utf8");
 
-  return source
+  return source;
 }
