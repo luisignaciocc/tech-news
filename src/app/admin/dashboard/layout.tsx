79bb32ab1c3c2beee4e3c419eab93e8e7c7c5d7c
ErickBackDev <erickson17graterol@gmail.com>
2024-06-12 14:46:10 -0400

    add param

--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -1,7 +1,14 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+
 import { Tabs } from "@/components/ui/tabs";
 import { TabsContent } from "@/components/ui/tabs";
 
 import DashboardTabList from "./components/dashboard-tab-list";
+
+type TabContent = {
+  [key: string]: React.ReactNode;
+};
 
 export default function GroupedLayout({
   posts,
@@ -10,18 +17,23 @@ export default function GroupedLayout({
   posts: React.ReactNode;
   news: React.ReactNode;
 }>) {
+  const tabContent: TabContent = {
+    posts,
+    news,
+  };
+
+  const searchParams = useSearchParams();
+  const tab = searchParams.get("tab") || "posts";
+
   return (
     <div className="flex-1 space-y-4 p-8 pt-6">
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
       </div>
-      <Tabs defaultValue="posts" className="space-y-4">
+      <Tabs defaultValue={tab} className="space-y-4">
         <DashboardTabList />
-        <TabsContent value="posts" className="space-y-4">
-          {posts}
-        </TabsContent>
-        <TabsContent value="news" className="space-y-4">
-          {news}
+        <TabsContent value={tab} className="space-y-4">
+          {tabContent[tab]}
         </TabsContent>
       </Tabs>
     </div>
