c8e04c9b5f18a7dfd11f2a4ea083d8c02f6f9425
ErickBackDev <erickson17graterol@gmail.com>
2024-06-12 12:40:46 -0400

    finish layout

--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -4,10 +4,9 @@ import { TabsContent } from "@/components/ui/tabs";
 import DashboardTabList from "./components/dashboard-tab-list";
 
 export default function GroupedLayout({
-  children,
   posts,
+  news,
 }: Readonly<{
-  children: React.ReactNode;
   posts: React.ReactNode;
   news: React.ReactNode;
 }>) {
@@ -19,8 +18,10 @@ export default function GroupedLayout({
       <Tabs defaultValue="posts" className="space-y-4">
         <DashboardTabList />
         <TabsContent value="posts" className="space-y-4">
-          {children}
           {posts}
+        </TabsContent>
+        <TabsContent value="news" className="space-y-4">
+          {news}
         </TabsContent>
       </Tabs>
     </div>
