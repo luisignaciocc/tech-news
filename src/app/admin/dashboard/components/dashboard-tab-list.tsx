8be920709a5dbe2c047dc8bca8cd5e70e37c6878
ErickBackDev <erickson17graterol@gmail.com>
2024-06-12 14:59:29 -0400

    Revert "add param"
    This reverts commit 79bb32ab1c3c2beee4e3c419eab93e8e7c7c5d7c.

--- src/app/admin/dashboard/components/dashboard-tab-list.tsx
+++ src/app/admin/dashboard/components/dashboard-tab-list.tsx
@@ -1,32 +1,12 @@
-"use client";
-import { useRouter } from "next/navigation";
-
 import { TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function DashboardTabList() {
-  const router = useRouter();
-
-  const handleTabClick = (tab: string) => {
-    router.push(`/admin/dashboard?tab=${tab}`);
-  };
-
   return (
     <TabsList>
-      <TabsTrigger value="posts" onClick={() => handleTabClick("posts")}>
-        Posts
-      </TabsTrigger>
-      <TabsTrigger value="news" onClick={() => handleTabClick("news")}>
-        News
-      </TabsTrigger>
-      <TabsTrigger value="reports" onClick={() => handleTabClick("reports")}>
-        Reports
-      </TabsTrigger>
-      <TabsTrigger
-        value="notifications"
-        onClick={() => handleTabClick("notifications")}
-      >
-        Notifications
-      </TabsTrigger>
+      <TabsTrigger value="posts">Posts</TabsTrigger>
+      <TabsTrigger value="news">News</TabsTrigger>
+      <TabsTrigger value="reports">Reports</TabsTrigger>
+      <TabsTrigger value="notifications">Notifications</TabsTrigger>
     </TabsList>
   );
 }
