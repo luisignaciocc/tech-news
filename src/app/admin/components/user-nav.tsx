c721a406c6d5e053b6709ab4faf844ff1175021b
ErickBackDev <erickson17graterol@gmail.com>
2024-06-13 11:39:41 -0400

    fix details

--- src/app/admin/components/user-nav.tsx
+++ src/app/admin/components/user-nav.tsx
@@ -2,7 +2,7 @@
 import { signOut, useSession } from "next-auth/react";
 import { FaSignOutAlt } from "react-icons/fa";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -22,7 +22,6 @@ export function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
             <AvatarFallback>NT</AvatarFallback>
           </Avatar>
         </Button>
