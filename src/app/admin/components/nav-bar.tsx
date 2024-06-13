87e79d07508186a6b3fb28f8ab0d7c98d164cc51
ErickBackDev <erickson17graterol@gmail.com>
2024-06-13 10:12:24 -0400

    add icon to navbar

--- src/app/admin/components/nav-bar.tsx
+++ src/app/admin/components/nav-bar.tsx
@@ -1,13 +1,14 @@
+import Image from "next/image";
+
 import { MainNav } from "@/app/admin/components/main-nav";
 import { Search } from "@/app/admin/components/search";
-import TeamSwitcher from "@/app/admin/components/team-switcher";
 import { UserNav } from "@/app/admin/components/user-nav";
 
 function NavBar() {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <TeamSwitcher />
+        <Image src="/icon.png" alt="Logo" width={50} height={50} priority />
         <MainNav className="mx-6 hidden md:block" />
         <div className="ml-auto flex items-center space-x-4">
           <Search />
