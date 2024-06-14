656db9e08e44a314c9cbe74b2d4fed06a340e769
ErickBackDev <erickson17graterol@gmail.com>
2024-06-14 19:11:02 -0400

    fix buttons

--- src/app/admin/validation/components/tab-links.tsx
+++ src/app/admin/validation/components/tab-links.tsx
@@ -15,16 +15,16 @@ export default function TabLinks() {
     <div className="flex space-x-4">
       <Link
         href={`/admin/validation/topublish?page=${page || 1}`}
-        className={`mx-3 bg-primary hover:bg-white hover:text-black border border-black text-white font-bold py-2 px-6 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0 ${
-          lastPathPart === "topublish" ? "bg-white text-black" : ""
+        className={`bg-primary hover:bg-white hover:text-black border border-black font-bold py-2 px-6 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0 ${
+          lastPathPart === "topublish" ? "bg-white text-black" : "text-white"
         }`}
       >
         To Publish
       </Link>
       <Link
         href={`/admin/validation/deleted?page=${page || 1}`}
-        className={`mx-3 bg-primary hover:bg-white hover:text-black border border-black text-white font-bold py-2 px-6 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0 ${
-          lastPathPart === "deleted" ? "bg-white text-black" : ""
+        className={`bg-primary hover:bg-white hover:text-black border border-black font-bold py-2 px-6 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0 ${
+          lastPathPart === "deleted" ? "bg-white text-black" : "text-white"
         }`}
       >
         Deleted
