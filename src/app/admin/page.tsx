857178b3bf4238825678165b11c331507d7a0148
ErickBackDev <erickson17graterol@gmail.com>
2024-06-14 13:29:40 -0400

    uncomment lines from admin/page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,5 @@
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function AdminPage() {
-  // redirect("/admin/dashboard");
+  redirect("/admin/dashboard");
 }
