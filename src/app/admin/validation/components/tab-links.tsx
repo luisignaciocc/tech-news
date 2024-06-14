bc3dda29605af16ba012be83a59c6047e75992fa
ErickBackDev <erickson17graterol@gmail.com>
2024-06-14 19:15:57 -0400

    fix buttons

--- src/app/admin/validation/components/tab-links.tsx
+++ src/app/admin/validation/components/tab-links.tsx
@@ -1,11 +1,9 @@
 "use client";
 import Link from "next/link";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export default function TabLinks() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const page = searchParams.get("page");
 
   const pathParts = pathname.split("/");
 
@@ -14,7 +12,7 @@ export default function TabLinks() {
   return (
     <div className="flex space-x-4">
       <Link
-        href={`/admin/validation/topublish?page=${page || 1}`}
+        href={`/admin/validation/topublish?page=1`}
         className={`bg-primary hover:bg-white hover:text-black border border-black font-bold py-2 px-6 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0 ${
           lastPathPart === "topublish" ? "bg-white text-black" : "text-white"
         }`}
@@ -22,7 +20,7 @@ export default function TabLinks() {
         To Publish
       </Link>
       <Link
-        href={`/admin/validation/deleted?page=${page || 1}`}
+        href={`/admin/validation/deleted?page=1`}
         className={`bg-primary hover:bg-white hover:text-black border border-black font-bold py-2 px-6 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0 ${
           lastPathPart === "deleted" ? "bg-white text-black" : "text-white"
         }`}
