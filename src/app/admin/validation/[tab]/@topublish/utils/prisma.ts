a31e9dd6608aca9093bd834521c1ea604ee19124
ErickBackDev <erickson17graterol@gmail.com>
2024-06-17 17:08:22 -0400

    use Promise.all for queries

--- src/app/admin/validation/[tab]/@topublish/utils/prisma.ts
+++ src/app/admin/validation/[tab]/@topublish/utils/prisma.ts
@@ -3,28 +3,28 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function getToPublishData(page: number, perPage: number) {
-  const totalCount = await prisma.news.count({
-    where: {
-      sentToApproval: true,
-      filtered: false,
-      deletedAt: null,
-    },
-  });
-
-  const offset = (page - 1) * perPage;
-  const data = await prisma.news.findMany({
-    where: {
-      sentToApproval: true,
-      filtered: false,
-      deletedAt: null,
-    },
-    select: {
-      id: true,
-      title: true,
-    },
-    skip: offset,
-    take: perPage,
-  });
+  const [totalCount, data] = await Promise.all([
+    prisma.news.count({
+      where: {
+        sentToApproval: true,
+        filtered: false,
+        deletedAt: null,
+      },
+    }),
+    prisma.news.findMany({
+      where: {
+        sentToApproval: true,
+        filtered: false,
+        deletedAt: null,
+      },
+      select: {
+        id: true,
+        title: true,
+      },
+      skip: (page - 1) * perPage,
+      take: perPage,
+    }),
+  ]);
 
   const hasMorePages = totalCount > page * perPage;
 
