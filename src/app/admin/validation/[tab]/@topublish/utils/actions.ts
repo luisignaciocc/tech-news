8a21316103d66b7ff3a9d62a9685a834a54872dd
Luis Ignacio <luisignacioccp@gmail.com>
2024-06-18 18:06:58 -0400

    feat: Add TelegramBot integration for updating filtered news

--- src/app/admin/validation/[tab]/@topublish/utils/actions.ts
+++ src/app/admin/validation/[tab]/@topublish/utils/actions.ts
@@ -1,5 +1,6 @@
 "use server";
 import { PrismaClient } from "@prisma/client";
+import TelegramBot from "node-telegram-bot-api";
 
 const prisma = new PrismaClient();
 
@@ -11,6 +12,7 @@ export async function updateDeletedAt(newsId: string) {
       },
       data: {
         deletedAt: new Date(),
+        deletionReason: `Not approved by *Admin*`,
       },
     });
 
@@ -27,15 +29,68 @@ export async function updateDeletedAt(newsId: string) {
 }
 
 export async function updateFiltered(newsId: string) {
-  try {
-    await prisma.news.update({
+  const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+  if (!TOKEN) {
+    console.error("No telegram API keys found");
+    return {
+      success: false,
+      message: `No telegram API keys found`,
+    };
+  }
+
+  const bot = new TelegramBot(TOKEN);
+  const [article, filteredNews] = await Promise.all([
+    prisma.news.findUnique({
       where: {
         id: newsId,
       },
-      data: {
+    }),
+    prisma.news.count({
+      where: {
         filtered: true,
+        posts: {
+          none: {},
+        },
       },
-    });
+    }),
+  ]);
+
+  if (!article) {
+    return {
+      success: false,
+      message: `No se encontró la noticia con ID ${newsId}`,
+    };
+  }
+  try {
+    const promises: Promise<unknown>[] = [
+      prisma.news.update({
+        where: {
+          id: newsId,
+        },
+        data: {
+          filtered: true,
+        },
+      }),
+    ];
+    if (article.telegramChatId && article.telegramMessageId) {
+      promises.push(
+        bot.editMessageReplyMarkup(
+          { inline_keyboard: [] },
+          {
+            chat_id: article.telegramChatId,
+            message_id: +article.telegramMessageId,
+          },
+        ),
+      );
+      promises.push(
+        bot.editMessageText(`✅ (${filteredNews + 1}) ${article.title}`, {
+          chat_id: article.telegramChatId,
+          message_id: +article.telegramMessageId,
+          parse_mode: "Markdown",
+        }),
+      );
+    }
+    await Promise.all(promises);
 
     return {
       success: true,
