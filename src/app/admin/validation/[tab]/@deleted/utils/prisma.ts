75192cb1704df12e13ee84b0c7d6f9bab8e8c2bd
ErickBackDev <erickson17graterol@gmail.com>
2024-06-18 13:36:24 -0400

    add Promise.all to query

--- src/app/admin/validation/[tab]/@deleted/utils/prisma.ts
+++ src/app/admin/validation/[tab]/@deleted/utils/prisma.ts
@@ -3,28 +3,30 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function getDeletedData(page: number, perPage: number) {
-  const totalCount = await prisma.news.count({
-    where: {
-      deletedAt: {
-        not: null,
+  const [totalCount, data] = await Promise.all([
+    prisma.news.count({
+      where: {
+        deletedAt: {
+          not: null,
+        },
       },
-    },
-  });
+    }),
 
-  const data = await prisma.news.findMany({
-    where: {
-      deletedAt: {
-        not: null,
+    prisma.news.findMany({
+      where: {
+        deletedAt: {
+          not: null,
+        },
       },
-    },
-    select: {
-      id: true,
-      title: true,
-      deletionReason: true,
-    },
-    skip: (page - 1) * perPage,
-    take: perPage,
-  });
+      select: {
+        id: true,
+        title: true,
+        deletionReason: true,
+      },
+      skip: (page - 1) * perPage,
+      take: perPage,
+    }),
+  ]);
 
   const hasMorePages = totalCount > page * perPage;
 
