a974ee12c20323fc9215631bcb8dd9ba15355767
ErickBackDev <erickson17graterol@gmail.com>
2024-06-18 15:55:52 -0400

    add refresh

--- src/app/admin/validation/[tab]/@deleted/components/table-buttons.tsx
+++ src/app/admin/validation/[tab]/@deleted/components/table-buttons.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useRouter } from "next/navigation";
 import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
 
 import { deleteNews } from "../utils/actions";
@@ -7,22 +8,16 @@ interface TableButtonsProps {
   newsId: string;
 }
 
-const handleDelete = async (newsId: string) => {
-  try {
-    const response = await deleteNews(newsId);
-    if (response.success) {
-      alert(response.message);
-    } else {
-      alert(response.message);
-    }
-  } catch (error) {
-    alert(
-      "Ocurrió un error al eliminar la noticia. Por favor, inténtalo de nuevo más tarde.",
-    );
-  }
-};
-
 function TableButtons({ newsId }: TableButtonsProps) {
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    try {
+      await deleteNews(newsId);
+      router.refresh();
+    } catch (error) {}
+  };
+
   return (
     <div className="flex justify-end space-x-2">
       <button className="flex items-center space-x-2 bg-green-500 hover:bg-green-600 text-white px-2 py-2 rounded">
@@ -30,7 +25,7 @@ function TableButtons({ newsId }: TableButtonsProps) {
       </button>
       <button
         className="flex items-center space-x-2 bg-red-500 hover:bg-red-600 text-white px-2 py-2 rounded"
-        onClick={() => handleDelete(newsId)}
+        onClick={() => handleDelete()}
       >
         <FaTimesCircle />
       </button>
