f0d4dc5d946d70e4e136894a57cddd3ee75a2ef0
Luis Ignacio <luisignacioccp@gmail.com>
2024-06-19 09:39:12 -0400

    feat: Add TelegramBot integration for updating filtered news

--- src/app/admin/validation/[tab]/@topublish/utils/actions.ts
+++ src/app/admin/validation/[tab]/@topublish/utils/actions.ts
@@ -5,16 +5,71 @@ import TelegramBot from "node-telegram-bot-api";
 const prisma = new PrismaClient();
 
 export async function updateDeletedAt(newsId: string) {
+  const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+  if (!TOKEN) {
+    console.error("No telegram API keys found");
+    return {
+      success: false,
+      message: `No telegram API keys found`,
+    };
+  }
+
+  const bot = new TelegramBot(TOKEN);
   try {
-    await prisma.news.update({
-      where: {
-        id: newsId,
-      },
-      data: {
-        deletedAt: new Date(),
-        deletionReason: `Not approved by *Admin*`,
-      },
-    });
+    const [article, filteredNews] = await Promise.all([
+      prisma.news.findUnique({
+        where: {
+          id: newsId,
+        },
+      }),
+      prisma.news.count({
+        where: {
+          filtered: true,
+          posts: {
+            none: {},
+          },
+        },
+      }),
+    ]);
+
+    if (!article) {
+      return {
+        success: false,
+        message: `No se encontró la noticia con ID ${newsId}`,
+      };
+    }
+
+    const promises: Promise<unknown>[] = [
+      prisma.news.update({
+        where: {
+          id: newsId,
+        },
+        data: {
+          deletedAt: new Date(),
+          deletionReason: `Not approved by *Admin*`,
+        },
+      }),
+    ];
+
+    if (article.telegramChatId && article.telegramMessageId) {
+      promises.push(
+        bot.editMessageReplyMarkup(
+          { inline_keyboard: [] },
+          {
+            chat_id: article.telegramChatId,
+            message_id: +article.telegramMessageId,
+          },
+        ),
+      );
+      promises.push(
+        bot.editMessageText(`❌ (${filteredNews + 1}) ${article.title}`, {
+          chat_id: article.telegramChatId,
+          message_id: +article.telegramMessageId,
+          parse_mode: "Markdown",
+        }),
+      );
+    }
+    await Promise.all(promises);
 
     return {
       success: true,
@@ -39,29 +94,30 @@ export async function updateFiltered(newsId: string) {
   }
 
   const bot = new TelegramBot(TOKEN);
-  const [article, filteredNews] = await Promise.all([
-    prisma.news.findUnique({
-      where: {
-        id: newsId,
-      },
-    }),
-    prisma.news.count({
-      where: {
-        filtered: true,
-        posts: {
-          none: {},
-        },
-      },
-    }),
-  ]);
-
-  if (!article) {
-    return {
-      success: false,
-      message: `No se encontró la noticia con ID ${newsId}`,
-    };
-  }
   try {
+    const [article, filteredNews] = await Promise.all([
+      prisma.news.findUnique({
+        where: {
+          id: newsId,
+        },
+      }),
+      prisma.news.count({
+        where: {
+          filtered: true,
+          posts: {
+            none: {},
+          },
+        },
+      }),
+    ]);
+
+    if (!article) {
+      return {
+        success: false,
+        message: `No se encontró la noticia con ID ${newsId}`,
+      };
+    }
+
     const promises: Promise<unknown>[] = [
       prisma.news.update({
         where: {
@@ -72,6 +128,7 @@ export async function updateFiltered(newsId: string) {
         },
       }),
     ];
+
     if (article.telegramChatId && article.telegramMessageId) {
       promises.push(
         bot.editMessageReplyMarkup(
