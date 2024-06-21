2ad32948006c01dd4380a85febebabc0d1e18c8e
ErickBackDev <erickson17graterol@gmail.com>
2024-06-21 15:26:35 -0400

    fix loading.tsx

--- src/app/admin/validation/[tab]/@topublish/loading.tsx
+++ src/app/admin/validation/[tab]/@topublish/loading.tsx
@@ -16,24 +16,32 @@ export default function Loading() {
     <Table className="mt-5">
       <TableHeader>
         <TableRow>
-          <TableHead className="w-1/3 px-4">
-            <Skeleton width={100} />
+          <TableHead className="w-1/3 px-4 flex items-center">
+            <Skeleton width={15} className="mr-2" />
+            <Skeleton width={120} />
           </TableHead>
           <TableHead className="w-1/6 text-right">
-            <Skeleton width={100} />
+            <div className="flex justify-end space-x-2">
+              <div className="mr-2">
+                <Skeleton width={120} />
+              </div>
+              <Skeleton width={25} height={25} />
+              <Skeleton width={25} height={25} />
+            </div>
           </TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {[...Array(4)].map((_, index) => (
+        {[...Array(10)].map((_, index) => (
           <TableRow key={index}>
-            <TableCell className="font-medium px-4">
-              <Skeleton />
+            <TableCell className="font-medium px-4 flex">
+              <Skeleton width={15} className="mr-2" />
+              <Skeleton width={800} />
             </TableCell>
             <TableCell className="text-right px-4">
               <div className="flex justify-end space-x-2">
-                <Skeleton width={40} height={30} />
-                <Skeleton width={40} height={30} />
+                <Skeleton width={25} height={25} />
+                <Skeleton width={25} height={25} />
               </div>
             </TableCell>
           </TableRow>
