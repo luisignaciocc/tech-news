300d66a315195a5569c43cda5578672286f1004b
ErickBackDev <erickson17graterol@gmail.com>
2024-06-27 14:59:45 -0400

    add pagination

--- src/app/admin/posts/page.tsx
+++ src/app/admin/posts/page.tsx
@@ -1,4 +1,7 @@
+import { Fragment } from "react";
+
 import PostsTable from "./components/deleted-table";
+import PaginationButtons from "./components/pagination-buttons";
 import { getPostsData } from "./utils/prisma";
 
 interface PostsData {
@@ -15,7 +18,16 @@ async function PostsPage({
   const perPage = 10;
   const response = await getPostsData(Number(searchParams?.page), perPage);
 
-  return <PostsTable data={response.data as PostsData[]} />;
+  return (
+    <Fragment>
+      <PostsTable data={response.data as PostsData[]} />
+      <PaginationButtons
+        page={Number(searchParams?.page)}
+        hasMorePages={response.hasMorePages}
+        tab={"deleted"}
+      />
+    </Fragment>
+  );
 }
 
 export default PostsPage;
