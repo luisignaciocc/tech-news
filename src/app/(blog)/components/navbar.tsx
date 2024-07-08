367002e0faba08b28aa6fff44394077a09ed8762
ErickBackDev <erickson17graterol@gmail.com>
2024-07-08 17:40:37 -0400

    Large screen design ready

--- src/app/(blog)/components/navbar.tsx
+++ src/app/(blog)/components/navbar.tsx
@@ -1,62 +1,48 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { BsSearch } from "react-icons/bs";
 
-function NavBar() {
+import { getMostUsedTags } from "@/lib/api";
+
+async function NavBar() {
+  const tags = await getMostUsedTags();
+
   return (
     <nav className="bg-[#333] shadow-[0_4px_12px_rgba(0,0,0,0.5)] fixed top-0 left-0 right-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-14">
           <div className="flex items-center">
-            <a
+            <Link
               href="/"
-              className="text-white hover:text-gray-400 text-xl font-bold"
+              className="text-white hover:text-gray-400 text-xl font-bold flex items-center"
             >
-              Ejemplo de Página
-            </a>
+              <div className="w-10 h-10 rounded-full overflow-hidden flex items-center mr-2">
+                <Image
+                  src="/icon.png"
+                  width="50"
+                  height="50"
+                  alt="Logotipo de Tecnobuc"
+                  className="w-[60px] h-[60px] object-cover"
+                />
+              </div>
+              Tecnobuc
+            </Link>
           </div>
-          <div className="flex items-center space-x-4">
-            <Link
-              href="/categoria1"
-              className="text-white hover:text-gray-400 px-3 py-2 rounded-md text-sm font-bold uppercase"
-            >
-              Categoría 1
-            </Link>
-            <span className="text-white">|</span>
-            <Link
-              href="/categoria2"
-              className="text-white hover:text-gray-400 px-3 py-2 rounded-md text-sm font-bold uppercase"
-            >
-              Categoría 2
-            </Link>
-            <span className="text-white">|</span>
-            <Link
-              href="/categoria3"
-              className="text-white hover:text-gray-400 px-3 py-2 rounded-md text-sm font-bold uppercase"
-            >
-              Categoría 3
-            </Link>
-            <span className="text-white">|</span>
-            <Link
-              href="/categoria4"
-              className="text-white hover:text-gray-400 px-3 py-2 rounded-md text-sm font-bold uppercase"
-            >
-              Categoría 4
-            </Link>
-            <span className="text-white">|</span>
-            <Link
-              href="/categoria5"
-              className="text-white hover:text-gray-400 px-3 py-2 rounded-md text-sm font-bold uppercase"
-            >
-              Categoría 5
-            </Link>
-            <span className="text-white">|</span>
-            <Link
-              href="/categoria6"
-              className="text-white hover:text-gray-400 px-3 py-2 rounded-md text-sm font-bold uppercase"
-            >
-              Categoría 6
-            </Link>
+          <div className="flex items-center space-x-1">
+            {tags.map((tag, index) => (
+              <div key={tag} className="flex items-center">
+                <Link
+                  href={`/posts/tags/${tag}`}
+                  className="text-white hover:text-gray-400 px-3 py-2 rounded-md text-sm font-bold uppercase"
+                >
+                  {tag}
+                </Link>
+                {index < tags.length - 1 && (
+                  <span className="text-white">|</span>
+                )}
+              </div>
+            ))}
             <span className="text-white">|</span>
             <button className="text-white hover:text-gray-400 px-3 py-2 rounded-md text-sm font-bold uppercase">
               <BsSearch className="h-6 w-6" />
