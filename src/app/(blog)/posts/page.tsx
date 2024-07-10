391ac4600886758bca70f838165a50f4e5820ff5
ErickBackDev <erickson17graterol@gmail.com>
2024-07-10 11:04:26 -0400

    fix details

--- src/app/(blog)/posts/page.tsx
+++ src/app/(blog)/posts/page.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-function SearchPost({ searchParams }) {
+interface SearchParams {
+  s: string;
+}
+
+function SearchPost({ searchParams }: { searchParams: SearchParams }) {
   return <div>Buscando... {searchParams.s}</div>;
 }
 
