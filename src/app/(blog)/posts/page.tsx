0c1cc6bd68d3110e0a9fbe36f06b87599d16cc42
ErickBackDev <erickson17graterol@gmail.com>
2024-07-12 10:33:37 -0400

    create MoreStoriesSection

--- src/app/(blog)/posts/page.tsx
+++ src/app/(blog)/posts/page.tsx
@@ -3,8 +3,7 @@ import React from "react";
 
 import { getPostsBySearchTerm, getRandomPosts } from "@/lib/api";
 
-import { MoreStories } from "../components/more-stories";
-import { PostPreview } from "../components/post-preview";
+import MoreStoriesSection from "../components/more-stories-section";
 import SpecialSection from "../components/special-section";
 
 interface SearchParams {
@@ -47,43 +46,13 @@ async function SearchPost({ searchParams }: { searchParams: SearchParams }) {
         </span>
       </div>
       <div className="flex gap-4 mt-2">
-        <div className="w-full lg:w-8/12 mt-6 lg:mt-14">
-          {morePosts.length > 0 ? (
-            <MoreStories
-              posts={morePosts.slice(0, 6)}
-              hasMorePosts={hasMorePosts}
-            />
-          ) : (
-            <div className="bg-gray-900 text-white w-full h-auto py-10 px-12">
-              <p className="text-2xl">No hay publicaciones disponibles.</p>
-              <p className="mt-5">Intenta con otro término de búsqueda.</p>
-            </div>
-          )}
-          {posts.length > 6 && (
-            <PostPreview
-              key={posts[6].slug}
-              title={posts[6].title}
-              coverImage={posts[6].coverImage}
-              date={posts[6].createdAt}
-              author={posts[6].author}
-              slug={posts[6].slug}
-              excerpt={posts[6].excerpt}
-              tags={posts[6].tags}
-            />
-          )}
-          {morePosts.length > 0 && (
-            <div className="mt-8">
-              <MoreStories
-                posts={morePosts.slice(7, 11)}
-                hasMorePosts={hasMorePosts}
-              />
-            </div>
-          )}
-        </div>
+        <MoreStoriesSection
+          morePosts={morePosts}
+          hasMorePosts={hasMorePosts}
+          posts={posts}
+        />
         <div className="w-4/12 hidden lg:block">
-          <div>
-            <SpecialSection specialPosts={specialPosts} />
-          </div>
+          <SpecialSection specialPosts={specialPosts} />
           <div className="mt-20">
             <h2 className="text-3xl uppercase">inteligencia artificial</h2>
             <hr className="bg-black border-1 border-black" />
