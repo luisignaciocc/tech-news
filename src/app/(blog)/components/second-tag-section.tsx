cc4ee68d24e9e6bd786beeb66eb34e436d8085e8
ErickBackDev <erickson17graterol@gmail.com>
2024-07-17 17:01:06 -0400

    adjusting SecondTagSection for mobile

--- src/app/(blog)/components/second-tag-section.tsx
+++ src/app/(blog)/components/second-tag-section.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 import ZoomImage from "./zoom-image";
 
@@ -29,45 +31,110 @@ function SecondTagSection({
   secondMostUsedTag,
   postsByTags,
 }: SecondTagSectionProps) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrevClick = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + postsByTags.length) % postsByTags.length,
+    );
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % postsByTags.length);
+  };
+
   return (
-    <div className="flex mb-16 mt-5">
-      <div className="w-8/12 h-full pr-2">
-        <div className="relative">
-          <ZoomImage
-            title={postsByTags[0].title}
-            src={postsByTags[0].coverImage || ""}
-            slug={postsByTags[0].slug}
-          />
-          <div className="absolute top-4 left-0 pl-6 pr-2 bg-black text-white py-1 rounded text-sm uppercase font-bold">
-            {secondMostUsedTag}
-          </div>
-          <Link href={`/posts/${postsByTags[0].slug}`}>
-            <div className="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-black to-transparent"></div>
-            <div className="absolute bottom-0 left-0 w-full px-4 pt-2 text-white overflow-hidden text-ellipsis line-clamp-3 hover:text-red-500">
-              {postsByTags[0].title}
+    <Fragment>
+      {/* Version para pantallas grandes */}
+      <div className="hidden lg:block">
+        <div className="flex mb-16 mt-5">
+          <div className="w-8/12 h-full pr-2">
+            <div className="relative">
+              <ZoomImage
+                title={postsByTags[0].title}
+                src={postsByTags[0].coverImage || ""}
+                slug={postsByTags[0].slug}
+              />
+              <div className="absolute top-4 left-0 pl-6 pr-2 bg-black text-white py-1 rounded text-sm uppercase font-bold">
+                {secondMostUsedTag}
+              </div>
+              <Link href={`/posts/${postsByTags[0].slug}`}>
+                <div className="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-black to-transparent"></div>
+                <div className="absolute bottom-0 left-0 w-full px-4 pt-2 text-white overflow-hidden text-ellipsis line-clamp-3 hover:text-red-500">
+                  {postsByTags[0].title}
+                </div>
+              </Link>
             </div>
-          </Link>
+          </div>
+          <div className="w-4/12 flex flex-col justify-between">
+            {postsByTags.slice(1).map((post, index) => (
+              <div key={index} className="relative">
+                <ZoomImage
+                  title={post.title}
+                  src={post.coverImage || ""}
+                  slug={post.slug}
+                />
+                <Link href={`/posts/${post.slug}`}>
+                  <div className="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-black to-transparent"></div>
+                  <div className="absolute bottom-0 left-0 w-full px-4 pt-2 text-white overflow-hidden text-ellipsis line-clamp-3 hover:text-red-500">
+                    {post.title}
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
 
-      <div className="w-4/12 flex flex-col justify-between">
-        {postsByTags.slice(1).map((post, index) => (
-          <div key={index} className="relative">
-            <ZoomImage
-              title={post.title}
-              src={post.coverImage || ""}
-              slug={post.slug}
-            />
-            <Link href={`/posts/${post.slug}`}>
-              <div className="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-black to-transparent"></div>
-              <div className="absolute bottom-0 left-0 w-full px-4 pt-2 text-white overflow-hidden text-ellipsis line-clamp-3 hover:text-red-500">
-                {post.title}
+      {/* Version para mobile */}
+      <div className="h-min mb-8 block lg:hidden">
+        <div className="carousel relative h-auto overflow-hidden">
+          <div className="carousel-items h-full">
+            {postsByTags.map((post, index) => (
+              <div
+                key={index}
+                className={`carousel-item h-full ${
+                  index === currentIndex ? "" : "hidden"
+                }`}
+              >
+                <div className="h-full flex items-center justify-center">
+                  <ZoomImage
+                    title={post.title}
+                    src={post.coverImage || ""}
+                    slug={post.slug}
+                  />
+                </div>
+                <div className="absolute top-4 left-0 pl-6 pr-2 bg-black text-white py-1 rounded text-sm uppercase font-bold sm:text-base">
+                  {secondMostUsedTag}
+                </div>
+                <Link href={`/posts/${post.slug}`}>
+                  <div className="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-black to-transparent"></div>
+                  <div className="absolute bottom-0 left-0 w-full px-4 pt-2 text-white overflow-hidden text-ellipsis line-clamp-3 hover:text-red-500 text-sm sm:text-base">
+                    {post.title}
+                  </div>
+                </Link>
               </div>
-            </Link>
+            ))}
           </div>
-        ))}
+          <div className="carousel-controls absolute top-1/2 -translate-y-1/2 w-full flex justify-between px-4 sm:px-6">
+            <button
+              id="prev-btn"
+              onClick={handlePrevClick}
+              className="bg-black/50 text-white p-2 rounded-full hover:bg-black sm:p-3"
+            >
+              <FaChevronLeft className="text-base sm:text-xl" />
+            </button>
+            <button
+              id="next-btn"
+              onClick={handleNextClick}
+              className="bg-black/50 text-white p-2 rounded-full hover:bg-black sm:p-3"
+            >
+              <FaChevronRight className="text-base sm:text-xl" />
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
 
