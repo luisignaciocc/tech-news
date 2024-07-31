c5d17cc92450c9ea999385e60a9b04baae38f347
Luis Ignacio <luisignacioccp@gmail.com>
2024-07-31 15:44:26 -0400

    chore: Refactor dashboard-navbar component and update dependencies
    Refactor the dashboard-navbar component to improve performance and fix styling issues. Changes include replacing the outer nav element with a div for better semantic structure, updating the background color and shadow styles, removing unnecessary classes and attributes, and updating the NavBar component props.
    
    This commit also includes the following dependency updates:
    - Bump react-day-picker from 9.0.5 to 9.0.6
    - Bump @types/node from 22.0.0 to 22.0.1
    - Bump eslint-config-turbo from 2.0.9 to 2.0.10

--- src/app/api/news/filter/route.ts
+++ src/app/api/news/filter/route.ts
@@ -119,10 +119,6 @@ export async function POST(request: Request): Promise<NextResponse> {
                       role: "system",
                       content: `
                       You are an assistant that classifies news. Your task is to determine if a news headline is related to technology.
-                      Do not consider reviews, opinion pieces, historical articles, deals, guides and tutorials, or articles related to movies, series, comics, or TV as technology news.
-                      If the headline refers to more than one topic, do not consider it as a technology news.
-                      If the headline is about a drop in the price of a product, do not consider it as a technology news.
-                      If the headline is about a startup valuation, aquisition, or funding, do not consider it as a technology news.
                       Respond with "yes" if it is related to technology, otherwise respond with "no" and explain the reasons why it is not considered a technology news.
                     `,
                     },
