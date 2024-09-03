c94921a461ca1382f5003b8b3bf7b2c75d199aae
Luis Ignacio <luisignacioccp@gmail.com>
2024-09-03 15:11:05 -0400

    chore: Update Analytics component to filter events by URL

--- src/app/components/analytics.tsx
+++ src/app/components/analytics.tsx
@@ -3,5 +3,14 @@
 import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
 
 export function Analytics() {
-  return <VercelAnalytics />;
+  return (
+    <VercelAnalytics
+      beforeSend={(event) => {
+        if (!event.url.endsWith(".com/")) {
+          return null;
+        }
+        return event;
+      }}
+    />
+  );
 }
