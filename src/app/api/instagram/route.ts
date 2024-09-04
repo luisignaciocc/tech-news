3404ecf68724397c1c5d9e6ab0afdbae4dd1c3ee
Luis Ignacio <luisignacioccp@gmail.com>
2024-09-04 11:13:54 -0400

    feat: Update take limit in POST function to retrieve 10 last posts
    Increase the take limit in the POST function of the route.ts file to retrieve 10 last posts instead of 7. This change ensures that more recent posts are fetched from the database.

--- src/app/api/instagram/route.ts
+++ src/app/api/instagram/route.ts
@@ -38,7 +38,7 @@ export async function POST(request: Request) {
       left join "News" n on n.id = p."newId" 
       where p."createdAt" >= NOW() - INTERVAL '24 HOURS' and p."postedToInstagram" = false
       order by similarity desc 
-      limit 7
+      limit 10
     `;
 
     const lastPosts = await prisma.post.findMany({
@@ -63,75 +63,85 @@ export async function POST(request: Request) {
       });
     }
 
-    const creationIds = [];
-    const articlesIds = [];
-    let articles = "";
-    let idx = 1;
-    for (const lastPost of lastPosts) {
-      const coverImageUrlEncoded = encodeURIComponent(
-        lastPost.coverImage || `${SITE_URL}/icon.png`,
-      );
+    const responses = await Promise.all(
+      lastPosts.map(async (lastPost) => {
+        const coverImageUrlEncoded = encodeURIComponent(
+          lastPost.coverImage || `${SITE_URL}/icon.png`,
+        );
 
-      const [completionTitle] = await Promise.all([
-        openai.chat.completions.create({
-          messages: [
-            {
-              role: "system",
-              content: `
-            Eres un asistente que crea titulos concisos y atractivos a partir de un artículo. 
-            Los titulos deben tener máximo 90 caracteres
-            Solo responde con el titulo del artículo, sin ninguna introducción o comentario adicional.
-          `,
-            },
-            {
-              role: "user",
-              content: `
-            Aquí tienes un artículo sobre tecnología:
-            ${lastPost.content}
-            
-            Por favor, proporciona un titulo adecuado.
-          `,
-            },
-          ],
-          model: "gpt-4o-mini",
-        }),
-      ]);
-
-      const title =
-        completionTitle?.choices?.[0]?.message?.content || lastPost.title;
-      const titleUrlEncoded = encodeURIComponent(title.replaceAll(`"`, ""));
-
-      const imageUrl = `${SITE_URL}/api/instagram/image?title=${titleUrlEncoded}&cover_image=${coverImageUrlEncoded}&api_key=${process.env.API_KEY}`;
-
-      const res = await fetch(
-        `https://graph.facebook.com/v19.0/${igUserId}/media`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          body: new URLSearchParams({
-            image_url: imageUrl,
-            is_carousel_item: "true",
-            access_token,
+        const [completionTitle] = await Promise.all([
+          openai.chat.completions.create({
+            messages: [
+              {
+                role: "system",
+                content: `
+              Eres un asistente que crea titulos concisos y atractivos a partir de un artículo. 
+              Los titulos deben tener máximo 90 caracteres
+              Solo responde con el titulo del artículo, sin ninguna introducción o comentario adicional.
+            `,
+              },
+              {
+                role: "user",
+                content: `
+              Aquí tienes un artículo sobre tecnología:
+              ${lastPost.content}
+              
+              Por favor, proporciona un titulo adecuado.
+            `,
+              },
+            ],
+            model: "gpt-4o-mini",
           }),
-        },
-      );
+        ]);
 
-      if (!res.ok) {
-        continue;
+        const title =
+          completionTitle?.choices?.[0]?.message?.content || lastPost.title;
+        const titleUrlEncoded = encodeURIComponent(title.replaceAll(`"`, ""));
+
+        const imageUrl = `${SITE_URL}/api/instagram/image?title=${titleUrlEncoded}&cover_image=${coverImageUrlEncoded}&api_key=${process.env.API_KEY}`;
+
+        const res = await fetch(
+          `https://graph.facebook.com/v19.0/${igUserId}/media`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: new URLSearchParams({
+              image_url: imageUrl,
+              is_carousel_item: "true",
+              access_token,
+            }),
+          },
+        );
+
+        if (!res.ok) {
+          return null;
+        }
+
+        const {
+          id,
+        }: {
+          id: string;
+        } = await res.json();
+        return {
+          creationId: id,
+          articleId: lastPost.id,
+          article: lastPost.content,
+        };
+      }),
+    );
+
+    const creationIds: string[] = [];
+    const articlesIds: string[] = [];
+    let articles = "";
+    responses.forEach((response) => {
+      if (response) {
+        creationIds.push(response.creationId);
+        articlesIds.push(response.articleId);
+        articles += `${response.article}\n\n`;
       }
-
-      const {
-        id,
-      }: {
-        id: string;
-      } = await res.json();
-      creationIds.push(id);
-      articlesIds.push(lastPost.id);
-      articles += `${idx}. ${title}\n`;
-      idx++;
-    }
+    });
 
     const completionDescription = await openai.chat.completions.create({
       messages: [
