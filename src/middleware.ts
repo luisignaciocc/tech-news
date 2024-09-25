4f544d01532e9133d05f90780d247882caa48743
ErickBackDev <erickson17graterol@gmail.com>
2024-09-25 10:47:43 -0400

    combine routing in middleware with protected route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,18 @@
-// export { default } from "next-auth/middleware";
-
-// export const config = {
-//   matcher: ["/admin/:path*"],
-// };
-
+import { default as nextAuthMiddleware } from "next-auth/middleware";
+import { NextRequestWithAuth } from "next-auth/middleware";
 import createMiddleware from "next-intl/middleware";
 
 import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+export default async function middleware(req: NextRequestWithAuth) {
+  // Calls the next-auth middleware
+  await nextAuthMiddleware(req);
+
+  // Calls the next-intl middleware
+  return createMiddleware(routing)(req);
+}
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ["/", "/(es|en)/:path*"],
+  // Combines the matchers
+  matcher: ["/", "/(es|en)/:path*", "/admin/:path*"],
 };
