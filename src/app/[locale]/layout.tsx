0a63cbf162563463a00b92ea64420895ff99abdb
ErickBackDev <erickson17graterol@gmail.com>
2024-09-26 11:09:52 -0400

    fix root layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,22 +3,19 @@ import "../globals.css";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from "next/font/google";
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
 
 import { Analytics } from "../components/analytics";
+import InternacionalizationProvider from "./(blog)/components/internacionalization-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
   params: { locale },
 }: Readonly<{
   children: React.ReactNode;
   params: { locale: string };
 }>) {
-  const messages = await getMessages();
-
   return (
     <html lang={locale}>
       <head>
@@ -27,9 +24,7 @@ export default async function RootLayout({
       <body className={inter.className}>
         <Analytics />
         <SpeedInsights />
-        <NextIntlClientProvider messages={messages}>
-          {children}
-        </NextIntlClientProvider>
+        <InternacionalizationProvider>{children}</InternacionalizationProvider>
       </body>
       <GoogleAnalytics gaId="G-0NYZMYCD8C" />
     </html>
