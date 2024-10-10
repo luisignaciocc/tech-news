5dc81a9b6f50e8f7eca09fdced85daa66bf84a1d
ErickBackDev <erickson17graterol@gmail.com>
2024-10-10 12:07:06 -0400

    Wrap all the content of SpecialCardPost in a single Link

--- src/app/[locale]/components/special-card-post.tsx
+++ src/app/[locale]/components/special-card-post.tsx
@@ -17,29 +17,30 @@ export const SpecialCardPost: React.FC<SpecialCardPostProps> = ({
 }) => {
   return (
     <Fragment>
-      <div className="flex flex-row items-start w-full mt-3">
-        <Link href={`/posts/${slug}`}>
-          <div className="w-24 h-24 rounded-md overflow-hidden mr-2 relative">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={imageUrl || ""}
-              alt={title}
-              className="w-full h-full object-cover"
-              width={100}
-              height={100}
-            />
-            <div className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-gray-900 to-transparent" />
-            <div className="absolute bottom-2 left-4 text-white text-2xl font-bold">
-              {number}
-            </div>
+      <Link
+        href={`/posts/${slug}`}
+        className="flex flex-row items-start w-full mt-3"
+      >
+        <div className="w-24 h-24 rounded-md overflow-hidden mr-2 relative">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={imageUrl || ""}
+            alt={title}
+            className="w-full h-full object-cover"
+            width={100}
+            height={100}
+          />
+          <div className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-gray-900 to-transparent" />
+          <div className="absolute bottom-2 left-4 text-white text-2xl font-bold">
+            {number}
           </div>
-        </Link>
+        </div>
         <div className="flex-1">
           <h3 className="mb-1 line-clamp-4 hover:text-gray-400 leading-tight tracking-tighter">
-            <Link href={`/posts/${slug}`}>{title}</Link>
+            {title}
           </h3>
         </div>
-      </div>
+      </Link>
     </Fragment>
   );
 };
