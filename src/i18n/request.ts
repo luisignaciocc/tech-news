2fc9bd73101b9309ded3c35ad15bf180b14a1c75
Luis Ignacio <luisignacioccp@gmail.com>
2024-10-21 23:53:59 -0300

    refactor(i18n): update locale handling and navigation imports

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,11 +1,16 @@
-import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
 
 import { routing } from "./routing";
 
-export default getRequestConfig(async ({ locale }) => {
+export default getRequestConfig(async ({ requestLocale }) => {
+  let locale = await requestLocale;
   // Validate that the incoming `locale` parameter is valid
-  if (!routing.locales.includes(locale as "en" | "es")) notFound();
+  if (
+    !locale ||
+    !routing.locales.includes(locale as (typeof routing.locales)[number])
+  ) {
+    locale = routing.defaultLocale;
+  }
 
   return {
     messages: (await import(`../../messages/${locale}.json`)).default,
--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,4 +1,4 @@
-import { createSharedPathnamesNavigation } from "next-intl/navigation";
+import { createNavigation } from "next-intl/navigation";
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
@@ -14,4 +14,4 @@ export type Locale = (typeof routing.locales)[number];
 // Lightweight wrappers around Next.js' navigation APIs
 // that will consider the routing configuration
 export const { Link, redirect, usePathname, useRouter } =
-  createSharedPathnamesNavigation(routing);
+  createNavigation(routing);
